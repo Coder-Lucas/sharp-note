2f4a94f5704bfd7da9048370e5fee051e1e01d7f
Coder-Lucas <lucas-2012@qq.com>
2025-12-14 21:32:40 +0800

    新增分析代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,14 @@ const metadata: Next.Metadata = {
     description: "SharpNote是一款开源在线笔记应用，使用Markdown格式，并且极其注重个人隐私，所有数据全部位于本机储存。"
 };
 
-const Layout: React.FC<{readonly children: React.ReactNode;}> = ({children}) => {
+const Layout: React.FC<{ readonly children: React.ReactNode; }> = ({children}) => {
     return (
         <html dir="ltr" lang="zh-cmn-Hans-CN">
             <body>
                 <header></header>
                 {children}
                 <footer></footer>
+                {process.env.ANALYSIS_TOKEN && <script dangerouslySetInnerHTML={{__html: `var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?${process.env.ANALYSIS_TOKEN}";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s);})();`}}></script>}
             </body>
         </html>
     );
