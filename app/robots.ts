3838ee7307d1495be96796603604c60efda47519
Coder-Lucas <lucas-2012@qq.com>
2026-01-09 20:30:29 +0800

    修复bug

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,17 @@
 import * as next from "next";
 
-const Robots: next.MetadataRoute.Robots = {
-    rules: {
-        allow: "/",
-        disallow: ["*#*", "*?*"],
-        userAgent: "*"
-    },
-    sitemap: "https://shnt.netlify.app/sitemap.xml"
+type TRobots = () => next.MetadataRoute.Robots;
+
+const Robots: TRobots = () => {
+    return {
+        host: "https://shnt.netlify.app/",
+        rules: {
+            allow: "/",
+            disallow: ["*#*", "*?*"],
+            userAgent: "*"
+        },
+        sitemap: "https://shnt.netlify.app/sitemap.xml"
+    };
 };
 
 export default Robots;
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,30 @@
 import * as next from "next";
 
-const Sitemap: next.MetadataRoute.Sitemap = [
-    {
-        lastModified: new Date(),
-        url: "https://shnt.netlify.app/"
-    },
-    {
-        lastModified: new Date(),
-        url: "https://shnt.netlify.app/func"
-    },
-    {
-        lastModified: new Date(),
-        url: "https://shnt.netlify.app/settings"
-    },
-    {
-        lastModified: new Date(),
-        url: "https://shnt.netlify.app/about"
-    },
-    {
-        lastModified: new Date(),
-        url: "https://shnt.netlify.app/docs"
-    }
-];
+type TSitemap = () => next.MetadataRoute.Sitemap;
+
+const Sitemap: TSitemap = () => {
+    return [
+        {
+            lastModified: new Date(),
+            url: "https://shnt.netlify.app/"
+        },
+        {
+            lastModified: new Date(),
+            url: "https://shnt.netlify.app/func"
+        },
+        {
+            lastModified: new Date(),
+            url: "https://shnt.netlify.app/settings"
+        },
+        {
+            lastModified: new Date(),
+            url: "https://shnt.netlify.app/about"
+        },
+        {
+            lastModified: new Date(),
+            url: "https://shnt.netlify.app/docs"
+        }
+    ];
+};
 
 export default Sitemap;
